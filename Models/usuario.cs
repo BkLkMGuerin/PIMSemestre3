20537911e75e92ba6e13a85b01c2327117e6829b
Miguel <miguel.guerino2011@gmail.com>
2026-04-11 02:24:49 -0300

    Primeiro projeto notebook

---
+++ Models/usuario.cs
@@ -0,0 +1,39 @@
+﻿using System.Dynamic;
+
+namespace novoprojeto;
+
+public class usuario
+{
+    public usuario(string name, string telefone)
+    {
+        Name = name;
+        Telefone = telefone;
+    }
+
+    private string _name = string.Empty;
+    private string _telefone = string.Empty;
+
+    public string Name
+    {
+        get => _name.ToUpper();
+        set
+        {
+            if (value == "")
+            {
+                throw new ArgumentException("Nome não deve ser vazio");
+            }
+            _name = value;
+        }
+    }
+    public string Telefone
+    {
+        get =>_telefone;
+        set
+        {
+            if (value == "")
+            {
+                throw new ArgumentException("Telefone não deve ser vazio");
+            }
+        }
+    }
+}
